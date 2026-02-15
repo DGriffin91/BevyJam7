ee127f456c85b6aa7b5f81483f0a0b1d11c865fa
DGriffin91 <github@dgdigital.net>
2026-02-14 23:11:40 -0800

    sweep underwater with reset

--- src/scene_underwater.rs
+++ src/scene_underwater.rs
@@ -1,12 +1,11 @@
 use avian3d::prelude::*;
-use bevy::{math::NormedVectorSpace, prelude::*, scene::SceneInstanceReady};
+use bevy::{prelude::*, scene::SceneInstanceReady};
 use bevy_fps_controller::controller::{FpsController, LogicalPlayer};
 
 use crate::{
     SceneContents, SceneState,
     cascade::{self, SceneBakeName},
     despawn_scene_contents,
-    draw_debug::DebugLines,
     physics::tri_mesh_collider,
     post_process::PostProcessSettings,
     prepare_lighting::DynamicLight,
@@ -21,7 +20,7 @@ impl Plugin for UnderwaterGameplayPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<PlayerUnderwaterState>().add_systems(
             Update,
-            (timed_events, move_airships, move_searchlights)
+            (teleporter, move_airships, move_searchlights)
                 .chain()
                 .run_if(in_state(SceneState::Underwater)),
         );
@@ -96,24 +95,6 @@ pub fn load_underwater(
         SceneBakeName(String::from("Underwater")),
     ));
 
-    let ship_scene = asset_server
-        .load(GltfAssetLabel::Scene(0).from_asset("testing/models/underwater_airship.gltf"));
-    for i in 0..4 {
-        let pos = SHIP_DESTINATIONS[i];
-        commands
-            .spawn((
-                SceneRoot(ship_scene.clone()),
-                UnderwaterScene,
-                SceneContents,
-                Transform::from_translation(vec3(pos.x, pos.y + i as f32 * 10.0, pos.z)),
-                Airship {
-                    destination: i,
-                    speed: i as f32 * 0.5 + 1.0,
-                },
-            ))
-            .observe(proc_ship);
-    }
-
     #[cfg(feature = "asset_baking")]
     ecmds.observe(
         |scene_ready: On<SceneInstanceReady>,
@@ -129,15 +110,28 @@ pub fn load_underwater(
             }
         },
     );
+
+    let ship_scene = asset_server
+        .load(GltfAssetLabel::Scene(0).from_asset("testing/models/underwater_airship.gltf"));
+    for i in 0..3 {
+        let pos = SHIP_DESTINATIONS[i];
+        commands
+            .spawn((
+                SceneRoot(ship_scene.clone()),
+                UnderwaterScene,
+                SceneContents,
+                Transform::from_translation(vec3(pos.x, pos.y + i as f32 * 10.0, pos.z)),
+                Airship {
+                    destination: i,
+                    speed: i as f32 * 0.5 + 1.0,
+                    index: i as u32,
+                },
+            ))
+            .observe(proc_ship);
+    }
 }
 
-fn timed_events(
-    mut commands: Commands,
-    time: Res<Time>,
-    asset_server: Res<AssetServer>,
-    mut graphs: ResMut<Assets<AnimationGraph>>,
-    camera: Single<&GlobalTransform, With<Camera>>,
-) {
+fn teleporter(mut commands: Commands, camera: Single<&GlobalTransform, With<Camera>>) {
     if camera.translation().z < -77.0 {
         commands.run_system_cached(despawn_scene_contents);
         commands.run_system_cached(load_hallway);
@@ -162,23 +156,40 @@ fn move_airships(mut airships: Query<(&mut Transform, &mut Airship)>, time: Res<
         }
         let dest_vec = to.normalize();
         let desired = trans.looking_at(current_dest, Vec3::Y).rotation;
-        let turn = 0.1;
+        let turn = 0.08;
         trans.rotation = trans
             .rotation
             .slerp(desired, 1.0 - (-turn * time.delta_secs()).exp());
         let align = dest_vec.dot(*trans.forward()).clamp(0.0, 1.0);
-        trans.translation += dest_vec * (8.0 * align * ship.speed) * time.delta_secs();
+        trans.translation += dest_vec * (6.0 * align * ship.speed) * time.delta_secs();
     }
 }
 
 fn move_searchlights(
-    mut search_lights: Query<(&ChildOf, &GlobalTransform, &mut Transform), With<Searchlight>>,
+    mut search_lights: Query<(&ChildOf, &GlobalTransform, &mut Transform, &Searchlight)>,
     parents: Query<&GlobalTransform>,
+    camera: Single<&GlobalTransform, With<Camera>>,
+    time: Res<Time>,
+    mut commands: Commands,
 ) {
-    let ws_aim_point = Vec3::ZERO;
-    for (parent, light_global, mut light_trans) in &mut search_lights {
+    let camera_pos = camera.translation();
+    let sweep_speed = 0.3;
+    let phase_strength = 1.0;
+    let sweep_distance = 100.0;
+    for (parent, light_global, mut light_trans, search_light) in &mut search_lights {
+        let z = SWEEP_REGIONS[search_light.index as usize];
+        let ws_aim_point = vec3(
+            (time.elapsed_secs() * sweep_speed + phase_strength * search_light.index as f32).sin()
+                * sweep_distance,
+            0.0,
+            z,
+        );
         let ws_light_pos = light_global.translation();
         let ws_dir = (ws_aim_point - ws_light_pos).normalize();
+        if point_in_cone(camera_pos, ws_light_pos, ws_dir, 9.0f32.to_radians()) {
+            commands.run_system_cached(despawn_scene_contents);
+            commands.run_system_cached(load_underwater);
+        }
         let desired_global = Transform::IDENTITY.looking_to(ws_dir, Vec3::Y).rotation;
         let parent_global_rot = parents.get(parent.0).unwrap().rotation();
         light_trans.rotation = parent_global_rot.inverse() * desired_global;
@@ -189,18 +200,23 @@ fn move_searchlights(
 struct Airship {
     destination: usize,
     speed: f32,
+    index: u32,
 }
 #[derive(Component, Clone, Debug, Default)]
-struct Searchlight;
+struct Searchlight {
+    index: u32,
+}
 
 const SHIP_DESTINATIONS: [Vec3; 6] = [
-    vec3(-40.0, 40.0, -80.0),
-    vec3(-45.0, 40.0, -60.0),
-    vec3(50.0, 40.0, -120.0),
-    vec3(45.0, 40.0, -60.0),
-    vec3(-40.0, 40.0, -170.0),
-    vec3(-50.0, 40.0, -120.0),
+    vec3(-40.0, 40.0, -40.0),
+    vec3(-45.0, 40.0, -20.0),
+    vec3(50.0, 40.0, -80.0),
+    vec3(45.0, 40.0, -20.0),
+    vec3(-40.0, 40.0, -120.0),
+    vec3(-50.0, 40.0, -90.0),
 ];
+
+const SWEEP_REGIONS: [f32; 3] = [-10.0, -40.0, -70.0];
 
 fn proc_ship(
     scene_ready: On<SceneInstanceReady>,
@@ -208,21 +224,38 @@ fn proc_ship(
     mut spot_lights: Query<&mut SpotLight>,
     mut commands: Commands,
     named: Query<(Entity, &Name)>,
+    airships: Query<&Airship>,
 ) {
-    for entity in children.iter_descendants(scene_ready.entity) {
-        if let Ok(mut spot_light) = spot_lights.get_mut(entity) {
-            spot_light.shadows_enabled = true;
-            spot_light.intensity *= 1000.0;
-            spot_light.range = 10000.0;
-            let mut ecmds = commands.entity(entity);
-            ecmds.insert(DynamicLight);
-            #[cfg(feature = "asset_baking")]
-            ecmds.insert(light_volume_baker::rt_scene::NoBake);
-        };
-        if let Ok((entity, name)) = named.get(entity) {
-            if name.contains("SEARCH_LIGHT") {
-                commands.entity(entity).insert(Searchlight);
+    if let Ok(airship) = airships.get(scene_ready.entity) {
+        for entity in children.iter_descendants(scene_ready.entity) {
+            if let Ok(mut spot_light) = spot_lights.get_mut(entity) {
+                spot_light.shadows_enabled = true;
+                spot_light.intensity *= 1000.0;
+                spot_light.range = 10000.0;
+                let mut ecmds = commands.entity(entity);
+                ecmds.insert(DynamicLight);
+                #[cfg(feature = "asset_baking")]
+                ecmds.insert(light_volume_baker::rt_scene::NoBake);
+            };
+            if let Ok((entity, name)) = named.get(entity) {
+                if name.contains("SEARCH_LIGHT") {
+                    commands.entity(entity).insert(Searchlight {
+                        index: airship.index,
+                    });
+                }
             }
         }
     }
+}
+
+fn point_in_cone(p: Vec3, origin: Vec3, normal: Vec3, opening: f32) -> bool {
+    let v = p - origin;
+    let v_len = v.length();
+    if v_len <= 1e-8 {
+        return true;
+    };
+    let dir = v / v_len;
+    let half_angle_rad = opening * 0.5;
+    let cos_half = half_angle_rad.cos();
+    return dir.dot(normal) >= cos_half;
 }
